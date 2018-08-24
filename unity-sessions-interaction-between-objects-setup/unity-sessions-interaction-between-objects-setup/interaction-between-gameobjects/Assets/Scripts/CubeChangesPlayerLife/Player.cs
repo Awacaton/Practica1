9c28458beecb4005be3bede57ce62cf91fc319fb
Awacaton <42677651+Awacaton@users.noreply.github.com>
2018-08-24 18:51:43 -0500

    Subir Primera Actividad

---
+++ unity-sessions-interaction-between-objects-setup/unity-sessions-interaction-between-objects-setup/interaction-between-gameobjects/Assets/Scripts/CubeChangesPlayerLife/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public int life = 10;
+    public Texture tex;
+        
+
+        void OnGUI()
+        {
+            GUI.Label(new Rect(10, 10, 100, 50), new GUIContent("Player: "+life, tex));
+        }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
